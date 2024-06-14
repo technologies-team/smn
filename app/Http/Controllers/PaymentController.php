682610b1d37b12d647e18cccbe465f065fea89d1
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-06-14 11:29:23 +0400

    food kitchen register notification

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -64,14 +64,15 @@ class PaymentController extends Controller
 
         // Handle the event
         switch ($event->type) {
+            case 'payment_intent.created':
+                $paymentIntent = $event->data->object;
+                Log::channel('payment')->info('PaymentIntent was created', ['payment_intent' => $event->id,'amount'=>$paymentIntent->amount]);
+                break;
             case 'payment_intent.succeeded':
                 $paymentIntent = $event->data->object;
                 Log::channel('payment')->info('PaymentIntent was successful!', ['payment_intent' => $paymentIntent->id,'amount'=>$paymentIntent->data->object->amount]);
                 break;
-                case 'payment_intent.created':
-                $paymentIntent = $event->data->object;
-                Log::channel('payment')->info('PaymentIntent was created', ['payment_intent' => $paymentIntent->id,'amount'=>$paymentIntent->data->object->amount]);
-                break;
+
             case 'payment_intent.payment_failed':
                 $paymentIntent = $event->data->object;
                 Log::channel('payment')->error('PaymentIntent failed', ['payment_intent' => $paymentIntent]);
