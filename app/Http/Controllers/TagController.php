30e49bcd5b9a10b1681b9a9c59f0b5d6ba59f57a
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-07-09 15:51:44 +0400

    update location country

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\DTOs\SearchQuery;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchRequest;
+use App\Http\Requests\StoreIngredientRequest;
+use App\Http\Responses\SuccessResponse;
 use App\Services\TagService;
+use Exception;
 use Illuminate\Http\Request;
 
 class TagController extends CrudController
@@ -11,5 +16,49 @@ class TagController extends CrudController
     protected TagService $service;
     public function __construct( TagService $service){
         $this->service=$service;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @param SearchRequest $request
+     * @return SuccessResponse
+     * @throws Exception
+     */
+    public function index(SearchRequest $request): SuccessResponse
+    {
+        return $this->ok($this->service->search(SearchQuery::fromJson($request->all())));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return SuccessResponse
+     * @throws Exception
+     */
+    public function show(int $id): SuccessResponse
+    {
+        return $this->ok($this->service->get($id));
+    }
+
+
+    public function updateAll(StoreIngredientRequest $request, int $id): SuccessResponse
+    {
+        return $this->ok($this->service->save($id, $request->all()));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function storeAll(StoreIngredientRequest $request): SuccessResponse
+    {
+        return $this->ok($this->service->create($request->all()));
+    }
+    /**
+     * @throws Exception
+     */
+    public function delete(int $id): SuccessResponse
+    {
+        return $this->ok($this->service->delete($id));
     }
 }
