e82f60736c764b1a03fc7b4c802395d9d140bf4e
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-07-15 15:04:44 +0400

    update tables foreign keys

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FeedBackRequest;
+use App\Http\Requests\OrderRequest;
 use App\Services\FeedBackService;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
@@ -11,8 +12,8 @@ use Illuminate\Http\Request;
 class OrderController extends CrudController
 {
     protected OrderService $service;
-    protected FeedBackRequest $Request;
-    public function __construct(OrderService $service,FeedBackRequest $Request){
+    protected OrderRequest $Request;
+    public function __construct(OrderService $service,OrderRequest $Request){
         $this->service=$service;
         $this->Request=$Request;
 
--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class FeedBackRequest extends FormRequest
+class OrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -55,9 +55,7 @@ class FeedBackRequest extends FormRequest
     protected function storeRules(): array
     {
         return [
-            'message' => 'max:20000|string',
-            'rate' => 'required|int',
-            'order_id' =>'required|int|exists:orders,id'
+
         ];
     }
 
