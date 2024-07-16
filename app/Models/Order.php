b72c814dcf008974b5ae56439ae3075b57ab90b4
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-07-16 09:49:03 +0400

    update tables foreign keys

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    const STATUS=['waiting','cooking','ready_to_delivery','cansel','reject','complete'];
 }
