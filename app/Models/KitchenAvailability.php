a470d7c5b2df9ec7dd356feb4bfc83ee670ffb6f
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-07-18 10:07:49 +0400

    add setting all kitchens

--- app/Models/KitchenAvailability.php
+++ app/Models/KitchenAvailability.php
@@ -10,5 +10,7 @@ class KitchenAvailability extends Model
     use HasFactory;
     protected $fillable = ['setting_id', 'day', 'is_available','start_time', 'end_time'];
     const DAYS=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
+    protected $casts = [
+        'is_available' => 'boolean',
+    ];
 }
